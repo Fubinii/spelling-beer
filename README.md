526e0ced2345da630bb0585e1951fa51e3ef75ab
Fubinii <fhw.schulz@outlook.com>
2026-02-26 13:31:04 +0100

    Put the game in its own function, edited readme

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # spelling-bee
-Recreating the NYT Spelling Bee Game
+This is a python version of the Spelling Bee Game, adapting the setup by the New York Times puzzle. Currently, the game can be played by running the spelling-bee.py file.
+
+You can use the provided dictionary 'en_US_60_SB.txt, which I tried to adjust so it replicates the word list of the NYT as closely as possible. It is based on the en_US.60 SCOWL word list, removing proper nouns, special characters and inappropriate language (the last one being a work-in-progress). I am also comparing it to the list of some of the NYT answers.
+You can use your own dictionary as well.
+
+To do:
+- Implement ranking system as in NYT
+- Create option to get a random set of letters in the beginning (with at least 2 vowels).
+
+
--- spelling_bee.py
+++ spelling_bee.py
@@ -1,123 +1,151 @@
 import random
 
+default_wordlist = 'en_US_60_SB.txt'
+
 # Loading the dictionary
-with open('en_US_60_edit.txt') as dic:
-    words = [w.strip() for w in dic if w.strip()]
+def load_wordlist(default_wordlist):
+    with open(default_wordlist) as dic:
+        word_list = [w.strip() for w in dic if w.strip()]
+    return word_list
 
-# Intro
-print("SPELLING BEE")
-print("------------")
-print("Type in letters to initialize game:")
-start = input().lower()
-letters = ''.join(dict.fromkeys(start))
-print('Available letters:', end=" ")
-for letter in letters:
-    print(letter.upper(), end=" ")
-print()
+def active_game(default_wordlist):
 
-# Center letter
-print("Chose a center letter:") 
-center_invalid = True
-while center_invalid:
-    center_letter = input().lower()
-    if center_letter not in letters:
-        print("Center letter must be one of the letters you initialized.")
-    else:
-        center_invalid = False
+    words = load_wordlist(default_wordlist)
+    # Intro
+    print("SPELLING BEE")
+    print("------------")
+    print("Current word list:", default_wordlist, "To change, type !wordlist <filename>")
+    start = input("Type in letters to initialize game: ").lower()
+    if start.startswith("!wordlist "):
+        filename = start.split(" ", 1)[1]
+        try: 
+            words = load_wordlist(filename)
+            start = input("Type in letters to initialize game: ").lower()
+        except FileNotFoundError:
+            print("File not found. Using default word list.")
+            start = input("Type in letters to initialize game: ").lower()
 
-# Initialize word list
-valid_words = []
-pangrams = []
-for word in words:
-    if len(word) >= 4 and center_letter in word and all(char in letters for char in word):
-        valid_words.append(word)
-        if all(char in word for char in letters): 
-            pangrams.append(word)
-
-# Determine score
-total_score = 0 
-for word in valid_words:
-    if len(word) == 4:
-        total_score += 1
-    else:
-        total_score += len(word) 
-print('Total points achievable:', total_score, 'including', len(pangrams), 'pangrams.')
-print('Start by typing a word. (For a list of commands type !help)')
-# Game loop
-found_words = []
-score = 0
-while True:
-    user_input = input().lower()
-
-    #=== COMANDS ===
-    if user_input.lower() == "!help":
-        print("!count - Show the number of valid words remaining")
-        print("!exit - Exit the game")
-        print("!found - Show already found words")
-        print("!hints - Show available hint commands")
-        print("!pangrams - Show how many pangrams there are")
-        print("!ranks - Show the points needed for each rank")
-        print("!reveal, !end - Reveal all valid words (ends the game)")
-        print("!shuffle - Shuffle the letters")
-        
-    elif user_input.lower() == "!count":
-        print(f"You found {len(found_words)} out of {len(valid_words)} possible words.")
-
-    elif user_input.lower() == "!exit":
-        break
-
-    elif user_input.lower() == "!found":
-        print("Found words:")
-        for word in found_words:
-            if word in pangrams:
-                print(word.upper(), end=", ")
-            else:
-                print(word, end=", ")
-        print()
-
-    # === HINT SECTION ===
-    elif user_input.lower() == "!hints":
-        print("!pangrams - Show how many pangrams there are. More hints will soon follow!")
-    
-    elif user_input.lower() == "!pangrams":
-        print(f"There are {len(pangrams)} possible pangrams with these letters.")
-    # ====================
-
-    elif user_input.lower() == "!ranks": 
-        print("Ranks will come in an upcoming version!")
-
-    elif user_input.lower() == "!reveal" or user_input.lower() == "!end":
-        print(f"Revealed at {score} / {total_score} points. Valid words were:")
-        for word in valid_words:
-            if word in found_words:
-                print('✓ ', word.upper())
-            else:
-                print(word)
-        break
-
-    elif user_input.lower() == "!shuffle":
-        l = list(letters)
-        random.shuffle(l)
-        letters = ''.join(l)
-
-    # === REGULAR GAME ===
-    else:
-        if user_input in valid_words and user_input not in found_words:
-            found_words.append(user_input)
-            score += 1 if len(user_input) == 4 else len(user_input)
-            if user_input in pangrams:
-                print("*** PANGRAM! *** Amazing!")
-            else:
-                print("Good!")
-        elif user_input in found_words:
-            print("Already found.")
-        else:
-            print("Not in word list.")
-    print('-', end=" ")
+    letters = ''.join(dict.fromkeys(start))
+    print('Available letters:', end=" ")
     for letter in letters:
-        if letter != center_letter:
-            print(letter.upper(), end=" - ")
-        else: 
-            print('[' + letter.upper() + ']', end=" - ")
-    print(f' Score: {score} / {total_score}') # To do: turn this into ranks
+        print(letter.upper(), end=" ")
+    print()
+
+    # Center letter
+    print("Chose a center letter:") 
+    center_invalid = True
+    while center_invalid:
+        center_letter = input().lower()
+        if center_letter not in letters:
+            print("Center letter must be one of the letters you initialized.")
+        else:
+            center_invalid = False
+
+    # Initialize word list
+    valid_words = []
+    pangrams = []
+    for word in words:
+        if len(word) >= 4 and center_letter in word and all(char in letters for char in word):
+            valid_words.append(word)
+            if all(char in word for char in letters): 
+                pangrams.append(word)
+
+    # Determine score
+    total_score = 0 
+    for word in valid_words:
+        if len(word) == 4:
+            total_score += 1
+        else:
+            total_score += len(word) 
+            if word in pangrams: total_score += 7 # Bonus points for pangrams
+
+    print('Total points achievable:', total_score, 'including', len(pangrams), 'pangrams.')
+    print('Start by typing a word. (For a list of commands type !help)')
+    # Game loop
+    found_words = []
+    score = 0
+    while True:
+        user_input = input().lower()
+
+        #=== COMANDS ===
+        if user_input.lower() == "!help":
+            print("!count - Show the number of valid words remaining")
+            print("!exit - Exit current game")
+            print("!found - Show already found words")
+            print("!hints - Show available hint commands")
+            print("!pangrams - Show how many pangrams there are")
+            print("!ranks - Show the points needed for each rank")
+            print("!reveal, !end - Reveal all valid words (ends the game)")
+            print("!shuffle - Shuffle the letters")
+            
+        elif user_input.lower() == "!count":
+            print(f"You found {len(found_words)} out of {len(valid_words)} possible words.")
+
+        elif user_input.lower() == "!exit":
+            break
+
+        elif user_input.lower() == "!found":
+            print("Found words:")
+            for word in found_words:
+                if word in pangrams:
+                    print(word.upper(), end=", ")
+                else:
+                    print(word, end=", ")
+            print()
+
+        # === HINT SECTION ===
+        elif user_input.lower() == "!hints":
+            print("!pangrams - Show how many pangrams there are. More hints will soon follow!")
+        
+        elif user_input.lower() == "!pangrams":
+            print(f"There are {len(pangrams)} possible pangrams with these letters.")
+        # ====================
+
+        elif user_input.lower() == "!ranks": 
+            print("Ranks will come in an upcoming version!")
+
+        elif user_input.lower() == "!reveal" or user_input.lower() == "!end":
+            print(f"Revealed at {score} / {total_score} points. Valid words were:")
+            for word in valid_words:
+                if word in found_words:
+                    print('✓ ', word.upper())
+                else:
+                    print(word)
+            break
+
+        elif user_input.lower() == "!shuffle":
+            l = list(letters)
+            random.shuffle(l)
+            letters = ''.join(l)
+
+        # === REGULAR GAME ===
+        else:
+            if user_input in valid_words and user_input not in found_words:
+                found_words.append(user_input)
+                score += 1 if len(user_input) == 4 else len(user_input)
+                if user_input in pangrams:
+                    print("*** PANGRAM! *** Amazing!")
+                    score += 7
+                else:
+                    print("Good!")
+            elif user_input in found_words:
+                print("Already found.")
+            else:
+                print("Not in word list.")
+        print('-', end=" ")
+        for letter in letters:
+            if letter != center_letter:
+                print(letter.upper(), end=" - ")
+            else: 
+                print('[' + letter.upper() + ']', end=" - ")
+        print(f' Score: {score} / {total_score}') # To do: turn this into ranks
+
+while True:
+
+    active_game(default_wordlist)
+
+    print("New game? (y/n)")
+    if input().lower() != 'y':
+        break
+
 
